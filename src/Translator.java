cdc9a2fb658a1ddf1534ceddf7049a64dc957a30
ali-saad-dev <aliisaadd5@gmail.com>
2023-09-29 13:49:31 +0200

    Work in Progress

--- src/Translator.java
+++ src/Translator.java
@@ -1,12 +1,18 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Translator {
     Map<Integer, String> numericAlpha = new HashMap<>();
 
     Translator(String[] alphabetic, Integer[] numeric) {
-        for (int i = 0; i < numericAlpha.size(); i++){
+        for (int i = 0; i < numericAlpha.size(); i++) {
             numericAlpha.put(10,"tien");
         }
     }
+
+   public String translate(Integer number, String value) {
+    return value;
+   }
+
 }
